5621e0031a683cc320d75b674da0e532cb94b896
Suras Kariyil <suraskariyil.com>
2024-11-08 11:44:03 +0530

    product module covered

--- ecom/products/urls.py
+++ ecom/products/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='home'),
     path('product_list',views.list_products,name='list_product'),
-    path('product_detail',views.detail_product,name='detail_product')
+    path('product_details/<pk>',views.detail_product,name='detail_product')
 
 ]
 
